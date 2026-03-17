3cadf31f3e10319e190c2af8b531ea0233e96d51
Sairam9107 <sairamn0707@gmail.com>
2026-03-17 08:41:18 +0530

    UC 1 - Application Entry & Welcome Message

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+class BookMyStayApp {
+    public static void main(String[] args) {
+        // Print welcome message and application details
+        System.out.println("=======================================");
+        System.out.println("   Welcome to Book My Stay App!");
+        System.out.println("   Hotel Booking System v1.0");
+        System.out.println("=======================================");
     }
 }
